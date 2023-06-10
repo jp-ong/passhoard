79e7b394c30c4e96423e23c8b8718733b97df644
John Paul Ong <jpong5202@gmail.com>
2023-06-10 22:12:56 +0800

    Add static home page content

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -14,14 +14,46 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
         actions: [
           IconButton(
-            icon: Icon(Icons.settings_rounded),
+            icon: const Icon(Icons.settings_rounded),
             onPressed: () {},
           ),
         ],
         leading: IconButton(
-          icon: Icon(Icons.lock_rounded),
+          icon: const Icon(Icons.lock_rounded),
           onPressed: () {},
         ),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: SizedBox(
+              height: 40,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search_rounded, size: 16),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              itemCount: 5,
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 8);
+              },
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: const Text("Password Group"),
+                  subtitle: Text(DateTime.now().toIso8601String()),
+                  trailing: const Icon(Icons.chevron_right_rounded),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
