5a8e30f8997d7b89d7e2067e3f2881f99d574ae2
John Paul Ong <jpong5202@gmail.com>
2023-06-13 01:02:46 +0800

    Add credentials form content

--- lib/app/modules/credentials_form/views/credentials_form_view.dart
+++ lib/app/modules/credentials_form/views/credentials_form_view.dart
@@ -10,14 +10,97 @@ class CredentialsFormView extends GetView<CredentialsFormController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CredentialsFormView'),
-        centerTitle: true,
+        title: const Text('New Password'),
+        actions: [TextButton(onPressed: () {}, child: const Text('Save'))],
       ),
-      body: const Center(
-        child: Text(
-          'CredentialsFormView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: SizedBox(
+              height: 40,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: "Group Name",
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: context.theme.colorScheme.surface,
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Username",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      SizedBox(
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            suffixIcon: IconButton(
+                              icon: const Icon(
+                                Icons.visibility_rounded,
+                                size: 16,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      ),
+                      Visibility(
+                        visible: index + 1 == 2,
+                        child: const SizedBox(height: 4),
+                      ),
+                      Visibility(
+                        visible: index + 1 == 2,
+                        child: OutlinedButton(
+                          child: Text(
+                            'Remove',
+                            style: TextStyle(
+                              color: context.theme.colorScheme.error,
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 16);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: ElevatedButton.icon(
+              icon: const Icon(Icons.add_rounded),
+              label: const Text('Add Password'),
+              onPressed: () {},
+            ),
+          ),
+        ],
       ),
     );
   }
